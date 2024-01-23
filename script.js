994d97348971a58dd9dd54ecb411be552f0a29fa
Pamilerin Oyewole <pamilerinoyewole06@gmail.com>
2024-01-23 14:48:29 +0100

    added responsiveness for small laptop screens

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const wrapper = document.querySelector(".wrapper");
 const loginLink = document.querySelector(".login-link");
 const registerLink = document.querySelector(".register-link");
 const btnPopup = document.querySelector(".btn-login-popup");
-const close = document.querySelector(".icon-close")
+const close = document.querySelector(".icon-close");
+const menuIcon= document.querySelector(".menu-icon");
+const navigationLinks = document.querySelector(".navigation")
 
 btnPopup.addEventListener("click", () => {
   wrapper.classList.add("btn-active");
+  navigationLinks.classList.remove("menu-active");
 });
 
 registerLink.addEventListener("click", () => {
@@ -18,4 +21,8 @@ loginLink.addEventListener("click", () => {
 
 close.addEventListener("click", () => {
   wrapper.classList.remove("btn-active")
+})
+
+menuIcon.addEventListener("click", () => {
+  navigationLinks.classList.toggle("menu-active")
 })
